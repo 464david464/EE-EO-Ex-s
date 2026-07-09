99424009c9d6e6f959467423db7047fe85830500
464david464 <davidmeachem464@gmail.com>
2026-07-09 22:58:00 +0300

    Add calculations for intrinsic carrier concentration and voltage change in new ex11qu3.m

--- ex11/ex11qu3.m
+++ ex11/ex11qu3.m
@@ -1,3 +1,4 @@
+%============from question 2=========================
 N=1e19; %=Na=Nd [cm^-3]
 e=1.6e-19; % Charge of electron [C]
 Eg=1.11*e; % Energy Gap in [J] for Silicon in 300K
@@ -8,5 +9,8 @@ kT=k*T; % Thermal energy [J]
 A=(ni_rt^2) * exp(Eg/(k*300)); % [cm^-6]
 
 ni=sqrt(A) * exp(-Eg/(2*k*T)); % [cm^-3]  
-Vb=2*kT/(e) * log(N/ni) % [V]
-
+Vb=2*kT/(e) * log(N/ni); % [V]
+% ====================================================
+%% +++++++++++ new Voltage ++++++++++++++++++++++++++
+V=10*Vb *1e3; % [mV] New voltage for three Thermocouples
+DV_Over_DT=V/T % [mV/K] Voltage change per temperature change
